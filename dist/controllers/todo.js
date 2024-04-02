47c791a375e77a586dd5b1201c671d43a3f30ae3
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-04-02 18:12:40 -0300

    adds more CRUD operations

--- dist/controllers/todo.js
+++ dist/controllers/todo.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodos = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 const createTodo = (req, res, next) => {
@@ -10,3 +10,28 @@ const createTodo = (req, res, next) => {
     res.status(201).json({ message: 'Created the todo', createTodo: newTodo });
 };
 exports.createTodo = createTodo;
+const getTodos = (req, res, next) => {
+    res.status(200).json({ todos: TODOS });
+};
+exports.getTodos = getTodos;
+const updateTodos = (req, res, next) => {
+    const updatedText = req.body.text;
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('Could not find todo!');
+    }
+    TODOS[todoIndex] = new todo_1.Todo(TODOS[todoIndex].id, updatedText);
+    res.json({ message: 'Updated', updateTodo: TODOS[todoIndex] });
+};
+exports.updateTodos = updateTodos;
+const deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('Could not find todo!');
+    }
+    TODOS.splice(todoIndex, 1);
+    res.json({ message: 'Todo deleted!' });
+};
+exports.deleteTodo = deleteTodo;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,15 +1,15 @@
 import {Router} from 'express';
 
-import { createTodo } from '../controllers/todo';
+import { createTodo, getTodos, updateTodos, deleteTodo } from '../controllers/todo';
 
 const router = Router();
 
 router.post('/', createTodo);
 
-router.get('/');
+router.get('/', getTodos);
 
-router.patch('/:id');
+router.patch('/:id', updateTodos);
 
-router.delete('/:id');
+router.delete('/:id', deleteTodo);
 
 export default router;
